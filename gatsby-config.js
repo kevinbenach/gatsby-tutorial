0d5686dba3829a6c1f6d373be571adb0a3f0b6d5
Kevin Benach <82702980+kevinbenach@users.noreply.github.com>
2022-11-10 12:27:18 +0100

    Updated gatsby-config.js so gatsby-plugin-image can work

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,21 @@
  */
 module.exports = {
   plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
     {
